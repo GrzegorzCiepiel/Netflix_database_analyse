e879847909e94bba83e2d2fee438812e9075f2b0
Grzegorz Ciepiel <grzegorz@ciepiel.pl>
2024-01-20 20:57:44 +0100

    Start analyse production types

--- main.py
+++ main.py
@@ -54,18 +54,35 @@ print(f'Budgets IQR = {budget_iqr}')
 print(df['est_budget (USD)'].var())
 print(df['est_budget (USD)'].std())
 
-sns.boxplot(data=df, x='est_budget (USD)')
-plt.show()
-plt.clf()
+# sns.boxplot(data=df, x='est_budget (USD)')
+# plt.show()
+# plt.clf()
+
+# sns.histplot(data=df, x='est_budget (USD)')
+# plt.ylabel('Netflix productions')
+# plt.xlabel('Budgets in USD')
+# plt.show()
+# plt.clf()
+
+# df.budget_scope.value_counts().plot.pie()
+# plt.show()
+# plt.close()
 
 print('\n')
 print(' CONCLUSION\nBudgets under two million dollars constitute only one percent of the productions.\n'
       'Medium budgets up to 50 milion dolars are most common and contain a half of all productions.\n'
       'Top ten percent of all productions are those with budget over 90 mln dollars. \n'
       'Mean budget is close to 50 mln dollars with median equal to 49,3 mln dollars.\n'
-      'Trimming the most extreme data points does not affect the mean value.')
+      'Trimming the most extreme data points does not affect the mean value.\n')
 
 # print('Lets dig into empty values \n')
 #
 # print(df.isnull().sum())
 # print(df[df.isnull().any(axis=1)])
+
+
+# Production types:
+print(df.type.unique())
+
+print(df.type.value_counts())
+print(df.type.value_counts(normalize=True))
