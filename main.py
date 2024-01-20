bc26f23d23365022e3cc5434ef195c767b86a586
Grzegorz Ciepiel <grzegorz@ciepiel.pl>
2024-01-20 12:21:15 +0100

    remove "no_data" cose there is no empty values

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print(df.rating.unique())
 
 print(df.type.unique())
 dummies_df = pd.get_dummies(data=df, columns=['type'])
-print(dummies_df.head(2))
+print(dummies_df.head(3))
 print('\n')
 
 # Add column with four budget scopes to easier group  by budgets.
@@ -23,23 +23,23 @@ print(f"Maximum budget is equal to: {df['est_budget (USD)'].max()}$")
 budget_scopes = ['small', 'medium', 'large', 'huge']
 
 def sort_budgets(budget):
-        if budget <= 700000:
+        if df['est_budget (USD)'].min() <= budget < 2000000:
             return 'small'
-        elif 700000 < budget <= 30000000:
+        elif 2000000 <= budget < 50000000:
             return 'medium'
-        elif 30000000 < budget <= 80000000:
+        elif 50000000 <= budget < 90000000:
             return 'big'
-        elif df['est_budget (USD)'].min() > budget:
-            return 'no data'
         else:
             return 'huge'
 
 df['budget_scope'] = df['est_budget (USD)'].apply(lambda x: sort_budgets(x))
 
-
 print(df.head(5))
+print(df.budget_scope.value_counts())
+print(df.budget_scope.isnull().sum())
 
-print('Lets dig into empty values \n')
 
-print(df.isnull().sum())
-print(df[df.isnull().any(axis=1)])
+# print('Lets dig into empty values \n')
+#
+# print(df.isnull().sum())
+# print(df[df.isnull().any(axis=1)])
